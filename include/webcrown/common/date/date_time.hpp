8f1b8a29b8aed730bae8ff3b553ae459c0250d5b
Alexsander Soares <alexsanderlimare@gmail.com>
2023-03-07 13:06:39 -0300

    initial commit

---
+++ include/webcrown/common/date/date_time.hpp
@@ -0,0 +1,81 @@
+#include "date/date.h"
+
+class date_time
+{
+    date::year_month_day ymd_;
+    std::chrono::time_point<std::chrono::system_clock, std::chrono::nanoseconds> timepoint_;
+public:
+    date_time()
+    {
+        auto tp = std::chrono::system_clock::now();
+        auto dp = date::floor<date::days>(tp);
+        ymd_ = date::year_month_day{dp};
+        timepoint_ = tp;
+        // = date::make_time(tp - dp);
+    }
+
+    date_time(date::year_month_day ymd, decltype(timepoint_) time)
+        : ymd_(ymd)
+        , timepoint_(time)
+    {}
+
+    date::year_month_day ymd() const noexcept { return ymd_; }
+    decltype(timepoint_) time() const noexcept { return timepoint_; }
+
+    std::string str() const
+    {
+        std::ostringstream os;
+
+        auto time = date::make_time(timepoint_ - date::sys_days(ymd_));
+
+        auto d = date::sys_time<decltype(time.to_duration())>(time.to_duration());
+
+        os 
+            << date::format("%Y-%m-%d ", ymd_)
+            << date::format("%X", d);
+
+        return os.str();
+    }
+
+    u_int32_t days_since(date_time const& rhs)
+    {
+        auto diff = this->timepoint_ - rhs.timepoint_;
+        auto&& result = std::chrono::duration_cast<date::days>(diff).count();
+
+        return result;
+    }
+
+    static date_time from_str(string const& s)
+    {
+        date::sys_time<std::chrono::nanoseconds> tp;
+
+        std::istringstream is{s};
+        is >> date::parse("%Y-%m-%d %X", tp);
+        auto dp = date::floor<date::days>(tp);
+        date::year_month_day ymd = date::year_month_day{dp};
+        //date::hh_mm_ss<std::chrono::nanoseconds> time = date::make_time(tp - dp);
+
+        return date_time{ymd, tp};
+    }
+
+    date_time& operator-(date_time const& rhs)
+    {
+        auto ymd_r = std::chrono::duration_cast<date::days>(date::sys_days(this->ymd_) - date::sys_days(rhs.ymd_));
+
+        auto diff = this->timepoint_ - rhs.timepoint_;
+        auto tp = date::sys_time<std::chrono::nanoseconds>(date::sys_days(this->ymd_).time_since_epoch() - diff);
+        auto dp = date::floor<date::days>(tp);
+        auto ymd = date::year_month_day{dp};
+
+        this->ymd_ = ymd;
+        this->timepoint_ = tp;
+
+        return *this;
+    }
+
+};
+
+inline std::ostream& operator<<(std::ostream& os, date_time const dt)
+{
+    return os << dt.str();
+}
---
+++ include/webcrown/orm/databases/postgres/enums.hpp
@@ -0,0 +1,52 @@
+
+
+enum class DataType
+{
+    bigint, // signed eight-byte integer
+    bigserial, // autoincrementing eight-byte integer
+    boolean, // logical boolean (true/false)
+    char_n, // fixed-length character string char[n]
+    cidr, // ipv4 or ipv6 network address
+    date, // calendar date (year, month, day)
+    integer,
+    decimal, // exact numeric of selectable precision
+    smallint, // int2, signed two-byte integer
+    smallserial, // serial2, autoincrementing two-byte integer
+    serial, // serial4, autoincremeting four-byte integer
+    text, // variable length character string
+    timestamp, // date and time (no time zone),
+    time, // time of day (no date)
+};
+
+enum class ColumnFlags : uint8_t
+{
+    none = 0b00000000,
+    primarykey = 0b00000001,
+    ignore_insert = 0b00000010,
+    ignore_on_serializer = 0b00000100
+};
+
+constexpr ColumnFlags operator|(ColumnFlags lhs, ColumnFlags rhs)
+{
+    return static_cast<ColumnFlags>(
+        static_cast<std::underlying_type_t<ColumnFlags>>(lhs) |
+        static_cast<std::underlying_type_t<ColumnFlags>>(rhs)
+    );
+};
+
+// constexpr ColumnFlags operator&(ColumnFlags lhs, ColumnFlags rhs)
+// {
+//     return static_cast<ColumnFlags>(
+//         static_cast<std::underlying_type_t<ColumnFlags>>(lhs) &
+//         static_cast<std::underlying_type_t<ColumnFlags>>(rhs)
+//     );
+// };
+
+// TODO: Fix constness
+constexpr ColumnFlags operator&(const ColumnFlags lhs, ColumnFlags rhs)
+{
+    return static_cast<ColumnFlags>(
+        static_cast<std::underlying_type_t<ColumnFlags>>(lhs) &
+        static_cast<std::underlying_type_t<ColumnFlags>>(rhs)
+    );
+}
